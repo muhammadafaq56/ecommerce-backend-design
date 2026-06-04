12fd3120e225e98148089c13888f9a387e1594c4
muhammadafaq56 <afaqkh22112006@gmail.com>
2026-06-04 22:53:51 +0500

    Filters

---
+++ Filters/UnreadMessagesFilter.cs
@@ -0,0 +1,37 @@
+﻿using ECommerceApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceApp.Filters
+{
+    public class UnreadMessagesFilter : IAsyncActionFilter
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UnreadMessagesFilter(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task OnActionExecutionAsync(
+            ActionExecutingContext context,
+            ActionExecutionDelegate next)
+        {
+            var routeData = context.RouteData.Values;
+
+            if (routeData["area"]?.ToString() == "Admin")
+            {
+                var count = await _context.ContactMessages
+                    .CountAsync(m => !m.IsReplied);
+
+                if (context.Controller is Controller controller)
+                {
+                    controller.ViewBag.UnreadMessages = count;
+                }
+            }
+
+            await next();
+        }
+    }
+}
